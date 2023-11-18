f738c300c4cd771a453e323b94ec9580e62b350f
wPatr <91604767+wPatr@users.noreply.github.com>
2023-11-18 17:37:35 +0100

    part2 done

--- Lab-Assignment_2/part2.c
+++ Lab-Assignment_2/part2.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -5,80 +6,157 @@
 #include <semaphore.h>
 #include <sys/mman.h>
 
+// struct Buffer is shares between the different processes
+struct Buffer{
+int VAR;
+int active_readers;    
+sem_t *sem_read;
+sem_t *sem_write;
+};
 
+const int MAX = 15; //constant representing the amximum value for the shared variable VAR
 
+//Declare methods for readers and writer
+void reader_method(struct Buffer *buffer_ptr);
+void writer_method(struct Buffer *buffer_ptr);
 
-
-sem_t sem_read_counter;
-sem_t sem_write_counter;
-
-void* create_shared_memory(size_t size) {
-  // Our memory buffer will be readable and writable:
-  int protection = PROT_READ | PROT_WRITE;
-
-  // The buffer will be shared (meaning other processes can access it), but
-  // anonymous (meaning third-party processes cannot obtain an address for it),
-  // so only this process and its children will be able to use it:
-  int visibility = MAP_SHARED | MAP_ANONYMOUS;
-
-  // The remaining parameters to `mmap()` are not important for this use case,
-  // but the manpage for `mmap` explains their purpose.
-  return mmap(NULL, size, protection, visibility, -1, 0);
-}
-
-
-void reader_method(){
-
-    sem_wait(&sem_read_counter);
-    
-    //read buffer
-    //print out PID, valueOfBuffer
-    pid_t process_id = getpid();
-    printf("The reader (%d) reads the value %d\n", process_id, 0);
-    sleep(1);
-
-    if(true){
-        printf("The last reader releases the lock.");
-    }
-    sem_post(&sem_read_counter);
-
-}
 
 int main(){
 
-    sem_init(&sem_read_counter, 0, 2);
-    sem_init(&sem_write_counter, 1, 1);
+    //Create semaphores for locking the shared variables during critical parts
+    sem_t *sem_read = sem_open("/sem_read", O_CREAT, S_IRUSR | S_IWUSR, 1);
+    sem_t *sem_write = sem_open("/sem_write", O_CREAT, S_IRUSR | S_IWUSR, 1);
+
+    //Create a shared memory for the processes
+    int shm_fd = shm_open("/shared_memory", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if(shm_fd == -1){
+        perror("shm_open");
+        exit(0);
+    }
+    //Set the size of the shared memory
+    if((ftruncate(shm_fd, sizeof(struct Buffer))) == -1){
+        perror("ftruncate");
+        exit(0);
+    }
+
+    // Map the shared memory to the struct Buffer
+    struct Buffer *buffer_ptr = mmap(NULL, sizeof(struct Buffer), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    //TODO - handle mmapp error
+
+    //Fork a child process
     pid_t pid = fork();
     if(pid == -1) {
     perror("fork");
     exit(1);
    } 
 
-    if(pid > 0){ // writer
+    if(pid >  0){ // Writer-process
         
-    }
+        writer_method(buffer_ptr);
 
+        //Wait for the Child process to finish
+        wait(NULL);
 
+        //Print the final value of VAR in the shared Buffer.
+        printf("VAR value: %d\n", buffer_ptr->VAR);
 
+        //Close and unlink the semaphores
+        sem_close(buffer_ptr->sem_read);
+        sem_close(buffer_ptr->sem_write);
+        sem_unlink("/sem_read");
+        sem_unlink("/sem_write");
 
-    if(pid == 0){ //Create two readers
+        //Unmap,close and unlink the shared memory
+        munmap(buffer_ptr, sizeof(struct Buffer));
+        close(shm_fd);
+        shm_unlink("/shared_memory");
 
-        pid_t pid = fork();
+        exit(EXIT_SUCCESS);
+        
+    }else{ //Reader processes
 
-        if(pid == -1) {
+        //Fork a Child process
+        pid_t pid2 = fork();
+
+        if(pid2 == -1) {
             perror("fork");
             exit(1);
             }
 
-        if(pid > 0){ //first reader
-            reader_method();
-        }   
+        if(pid2 > 0){ //First reader process
+            reader_method(buffer_ptr);
+            
+            //Wait for the Child process to finish before exiting process.
+            wait(NULL);
+            exit(EXIT_SUCCESS);
 
-        if(pid == 0){ // second reader
-            reader_method();
-
+        } else {  // Second reader process
+           reader_method(buffer_ptr);
+             exit(EXIT_SUCCESS);
         }
 
     
    }
+}
+
+//Reader method executed by two reader processes
+void reader_method(struct Buffer *buffer_ptr){
+    
+    while(buffer_ptr->VAR < MAX){
+  
+    //Aquire the reader semaphore to enter critical section,
+    //and then increment the ative_readers variable by one.
+    sem_wait(buffer_ptr -> sem_read); 
+    buffer_ptr -> active_readers++;  
+
+    // The first reader waits for the write semaphore to be available.
+    if(buffer_ptr -> active_readers == 1) 
+        sem_wait(buffer_ptr -> sem_write); 
+        printf("The first reader acquires the lock.\n");
+    
+    //Release the read semaphore to allow other readers.
+    sem_post(buffer_ptr -> sem_read); 
+ 
+    
+    //Reads the value of VAR from the shared buffer, and prints out the value together with PID
+    pid_t process_id = getpid();
+    printf("Reader (%d) reads the value %d\n", process_id, buffer_ptr->VAR);
+
+    //Decrement active_readers variable.
+    buffer_ptr -> active_readers--;    
+
+    //If this is the last reader, release the write semaphore to allow wother processes.
+    if(buffer_ptr -> active_readers == 0)
+        printf("The last reader releases the lock.\n");
+        sem_post(buffer_ptr->sem_write);
+        
+    //Sleep
+    usleep(200);
+    }
+
+}
+
+//Writer method executed by one writer process.
+void writer_method(struct Buffer *buffer_ptr){
+    pid_t process_id = getpid();
+
+    while(buffer_ptr->VAR < MAX){
+
+    //Aquire the writer semaphore to enter critical section,
+    //and then increment the VAR variable from the shared buffer by one.
+    sem_wait(buffer_ptr->sem_write);
+    printf("The writer acquires the lock.\n");
+    buffer_ptr->VAR++;
+
+    //Print the new VAR value together with the PID.
+    printf("The writer (%d) writes the value %d\n", process_id, buffer_ptr->VAR);
+
+    //Release the write semaphore to allow other processes.
+    printf("The writer releases the lock.\n");
+    sem_post(buffer_ptr->sem_write);
+
+    //Sleep
+    usleep(200);
+    }
 }
