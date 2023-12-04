d7c4b5a66e3c6e76a092a1a7fce714a15ae46892
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-04 08:46:40 +0100

    added basic code

--- Lab-Assignment_4/part1.c
+++ Lab-Assignment_4/part1.c
@@ -1,10 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
-int main(int argc, char *argv[]){
+#define CYLINDER_REQUESTS 1000
 
-int head_pointer = atoi(argv[1]);
-printf("Head pointer: %d\n", head_pointer);
+// Function declaration
+void FCFS(int head_position, int request_queue[]);
+void SSTF(int head_position, int request_queue[]);
+void SCAN(int head_position, int request_queue[]);
+void C_SCAN(int head_position, int request_queue[]);
+void LOOK(int head_position, int request_queue[]);
+void C_LOOK(int head_position, int request_queue[]);
 
 
+
+int main(int argc, char *argv[])
+{
+    //Extracts the argument from command line, and converts it to an int. 
+    int head_position = atoi(argv[1]);
+    printf("Head start position: %d\n", head_position);
+
+    
+    int request_queue[CYLINDER_REQUESTS];
+}
+
+
+
+
+// FCFS
+void FCFS(int head_position, int request_queue[])
+{
+
+    printf("Head movements required for FCFS: ");
+}
+
+// SSTF
+void SSTF(int head_position, int request_queue[])
+{
+
+    printf("Head movements required for SSTF: ");
+}
+
+// SCAN
+void SCAN(int head_position, int request_queue[])
+{
+
+    printf("Head movements required for SCAN: ");
+}
+
+// C-SCAN
+void C_SCAN(int head_position, int request_queue[])
+{
+    printf("Head movements required for C_SCAN: ");
+}
+
+// LOOK
+void LOOK(int head_position, int request_queue[])
+{
+    printf("Head movements required for LOOK: ");
+}
+
+// C-LOOK
+void C_LOOK(int head_position, int request_queue[])
+{
+    printf("Head movements required for C_LOOK: ");
 }
